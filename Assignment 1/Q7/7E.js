4be6e482332f922a6ac2b66ead39f08c8eb28c2e
Someshwar Mirge <mirgesomeshwar@gmail.com>
2021-04-08 15:36:27 +0530

    Update 7E.js

--- Assignment 1/Q7/7E.js
+++ Assignment 1/Q7/7E.js
@@ -5,14 +5,36 @@
 
 let readline = require("readline-sync");
 let n = parseInt(readline.question("Enter  N:"));
-let col = 0;
-let row = 0;
-while (row <= n) {
+let col = 1;
+let row = 1;
+let count = 1;
+let count1 = 1;
+while (row <= n + 1) {
     col = 1;
-    while (col <= row) {
-        process.stdout.write("* ");
+    count = 1;
+    if (row % 2 == 0) {
+        temp = row / 2;
+    } else {
+        temp = (row + 1) / 2;
+    }
+    while (col <= temp) {
+
+        process.stdout.write(String(count) + "   ");
+        count = count + jump(row, );
         col += 1;
     }
+    while (col > temp && col <= row) {
+        count1 = count - jump();
+        process.stdout.write(String(count1) + "   ");
+        col += 1;
+    }
+
     console.log("");
     row += 1;
+}
+
+function jump(row, ) {
+    let result = 0;
+
+    return result;
 }
