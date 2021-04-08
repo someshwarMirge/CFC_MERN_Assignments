4018432da20360a2b0e68125b6a817557d06ea01
Someshwar Mirge <mirgesomeshwar@gmail.com>
2021-04-08 14:57:11 +0530

    7H completed

--- Assignment 1/Q7/7H.js
+++ Assignment 1/Q7/7H.js
@@ -5,22 +5,50 @@
 
 let readline = require("readline-sync");
 let n = parseInt(readline.question("Enter  N:"));
-let col = 1;
-let row = 1;
-while (row < 2 * n) {
-    col = 1;
-    while (col < 2 * n) {
-        if (row <= n && col < ) {
+let col = 0;
+let row = 0;
+let count = n - 1;
+let count1 = -1;
+let count2 = n - 1;
+while (row < 2 * n - 1) {
+    col = 0;
+    while (col <= count) {
 
-        } else {
-            process.stdout.write("* ");
-        }
-
-
+        process.stdout.write("* ");
 
         col += 1;
     }
+    col = 0;
+    while (col < count1) {
 
-    console.log("");
+        process.stdout.write("- ");
+
+        col += 1;
+    }
+    col = 0;
+    while (col <= count2) {
+
+        process.stdout.write("* ");
+
+        col += 1;
+    }
+    process.stdout.write("\n");
+    if (row < n - 1) {
+        count -= 1;
+    } else {
+        count += 1;
+    }
+    if (row < n - 1) {
+        count1 += 2;
+    } else {
+        count1 -= 2;
+    }
+    if (row < n - 1) {
+        count2 -= 1;
+    } else {
+        count2 += 1;
+    }
+
+
     row += 1;
 }
